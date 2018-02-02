492c1c3286e75d3e9004886a76982220fb2aa01d
Travis CI <bobby.brennan@gmail.com>
2018-02-02 20:15:59 +0000

    create simple http server

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from model import Model
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
 mnist = input_data.read_data_sets('/tmp/MNIST_data')
 first_batch = mnist.train.next_batch(50)
 
 m = Model()
 encoding = [.5, .5]
-print m.decode(encoding)
+
+@app.route('/encode', methods=["POST"])
+def encode():
+  return jsonify(m.encode(request.json).tolist())
+
+@app.route('/decode', methods=["POST"])
+def decode():
+  return jsonify(m.decode(request.json).tolist())
 
