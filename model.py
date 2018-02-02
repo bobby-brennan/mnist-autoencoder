88220c2c3a9ca89dfa48852221175f14c6f07e63
Travis CI <bobby.brennan@gmail.com>
2018-02-02 19:59:08 +0000

    add model.py

---
+++ model.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+MODEL_DIR = "./model/model.ckpt"
+
+class Model(object):
+  def __init__(self):
+    tf.reset_default_graph()
+    self.session = tf.Session()
+    saver = tf.train.import_meta_graph(MODEL_DIR + '.meta')
+    saver.restore(self.session, MODEL_DIR)
+    self.decoded = self.session.graph.get_tensor_by_name('Decoded:0')
+    self.encoded = self.session.graph.get_tensor_by_name('Encoded:0')
+    self.x = self.session.graph.get_tensor_by_name('x:0')
+
+  def encode(self, image):
+    val = self.session.run(self.encoded, feed_dict={self.x: [image]})
+    return val[0]
+
+  def decode(self, encoding):
+    val = self.session.run(self.decoded, feed_dict={self.encoded: [encoding]})
+    return val[0]
+
