2258527dc8d4854698302e382dbc83aa156b8eff
Robert Brennan <bobby.brennan@gmail.com>
2020-02-18 01:31:08 +0000

    fix up requirements

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
+import os
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 from model import Model
 from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS, cross_origin
 import json
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+BROWSER_DIR = os.path.dirname(__file__) + "/web/dist/browser/"
+app = Flask(__name__, static_folder=BROWSER_DIR, static_url_path="/")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 strategy = "gan"
 MODEL_DIR = "./models/" + strategy + "/model.ckpt"
@@ -29,25 +29,29 @@ for i in range(0, NUM_SAMPLES):
     decoded = m.decode([labeled_images[i]['encoding']])
     labeled_images[i]['decoded'] = np.reshape(decoded[0], [28, 28]).tolist()
 
+@app.route('/reload_model')
+def reload_model():
+    m = Model(MODEL_DIR)
+    return jsonify(ok=True)
+
 @app.route('/images')
 def get_images():
   return jsonify(labeled_images)
 
 @app.route('/encode', methods=["POST"])
-@cross_origin()
 def encode():
   image = np.reshape(request.json, [28*28])
   return jsonify(m.encode([image])[0].tolist())
 
 @app.route('/decode', methods=["POST"])
-@cross_origin()
 def decode():
   val = m.decode([request.json])
   return jsonify(np.reshape(val[0], [28, 28]).tolist())
 
-@app.route('/<path:path>', methods=["GET"])
-def webserver(path):
-    return send_from_directory("web/dist/browser", path)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file("index.html")
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
